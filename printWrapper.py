c4396ec9694fd1261a518300456055c5f1bca666
had <heandong1992@qq.com>
2017-09-14 13:23:31 +0800

    好累啊

--- printWrapper.py
+++ printWrapper.py
@@ -1,11 +1,25 @@
+# coding=utf-8
 
+import Tkinter
 
-#todo
+ERROR = 'ERROR'
+WARN = 'WARN'
+SUCCESS = 'SUCCESS'
+NORMAL = 'NORMAL'
 
 class PrintWrapper:
-    def __init__(self, textConsolo):
-        self.of = outputFunc
+    def __init__(self):
+        self.tC = None
         pass
 
-    def myPrint(self, **info):
-        self.outputFunc(info['text'])
+    def setTextConsolo(self,textConsolo):
+        self.tC = textConsolo
+
+    def myPrint(self, infotype='', text=''):
+        self.tC.insert(Tkinter.END, infotype+' '+text+'\n')
+
+    def myPrintInfo(self, info):
+        for key, value in info.items():
+            if not key == 'text':
+                self.tC.insert(Tkinter.END, key + ':' + value + '\n')
+        self.tC.insert(Tkinter.END, info['text'] + '\n')
